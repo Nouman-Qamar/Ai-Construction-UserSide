55648de17506bbe827df3e7f7c24451802ad4e16
Nouman Qamar <155750361+Nouman-Qamar@users.noreply.github.com>
2025-12-21 22:27:09 +0500

    merge and correct the code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import FormFields from "./Components/FormField/FormFields.jsx";
+import { ButtonExamples } from "./Components/Button/Button.jsx";
+import CardsPage from "./Components/Cardpage/Cardpage.jsx";
+import Modalmenu from "./Components/Model/model.jsx";
+
 
 
 
@@ -11,7 +15,17 @@ function App() {
     <div className="app-layout">
      
       <Navbar />
+      {/* <main className="app-content">
+        <Routes>
+          <Route path="/" element={<FormFields />} />
+        </Routes>
+      </main> */}
 
+      {/* <ButtonExamples/> */}
+
+      {/* <CardsPage/> */}
+
+      <Modalmenu/>
 
       <Footer />
     </div>
